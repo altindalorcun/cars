7f9a9a8140aea28dffa06929b5dc066553696951
altindalorcun <altindalorcun@gmail.com>
2024-07-27 16:45:42 +0300

    Create Garage Service application

---
+++ garage-service/src/test/java/tr/com/altindalorcun/garage_service/GarageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.altindalorcun.garage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
